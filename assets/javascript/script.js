ef221b5857559be01e4096180b59a8ff4ce9f59d
Jakenbake20 <jacob_wheat@yahoo.com>
2020-04-13 21:18:55 -0600

    Added code for covid-19 api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://covid-19-data.p.rapidapi.com/totals?format=undefined",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+      "x-rapidapi-key": "08897b03bcmshc81efaa969f609ap1c87cdjsn9def16cda817"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
